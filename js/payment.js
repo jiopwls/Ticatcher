1d101e6731f8df1dcb4c84eb4376cc44a7cb8857
jiopwls <jiopwls@naver.com>
2023-01-25 21:00:40 +0900

    결제 페이지 작업 중

---
+++ js/payment.js
@@ -0,0 +1,19 @@
+$(function(){
+    $("#datepicker").datepicker({
+        minDate:0,
+        dateFormat: 'yy-mm-dd',
+        prevText: '이전 달',
+        nextText: '다음 달',
+        monthNames: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
+        monthNamesShort: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
+        dayNames: ['일', '월', '화', '수', '목', '금', '토'],
+        dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],
+        dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
+        showMonthAfterYear: true,
+        yearSuffix: '.',
+        buttonImageOnly: true
+    });
+    $('#datepicker').datepicker('setDate','today');
+});
+
+
